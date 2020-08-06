f2faf1c2a2bc9ae70b88fa2a3b6a588c8027e18d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-06 15:54:07 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,6 +3,9 @@ class Character < ActiveRecord::Base
   belongs_to :shows
   
   def say_that_thing_you_say
-    self.catchphrase
+    actor = self.actor
+    cp = self.catchphrase
+    
+    "#{actor} always says: #{cp}"
   end
 end
