80794051a392ac82a5947aa87eb9f7f2ab0bbfad
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-06 16:08:30 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day.rb
+++ db/migrate/007_add_day.rb
@@ -1,4 +1,4 @@
-class AddCatchphrase < ActiveRecord::Migration[5.1]
+class AddDay < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
